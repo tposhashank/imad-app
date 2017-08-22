b06fe892d6c28a5625de3d82008472e7522c6ebc
tposhashank <30650425+tposhashank@users.noreply.github.com>
2017-08-22 16:27:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,17 @@ element.innerHTML = 'NEW VALUE';
 
 //Changing image
 var img = document.getElementById('madi');
+//img.onclick = function(){
+//    img.style.marginLeft = '200px';
+//};
+
+//Adding motion to the image
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function(){
-    img.style.marginLeft = '200px';
-};
+var interval = setInterval(moveRight,50);
+}
