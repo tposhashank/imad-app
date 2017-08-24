46d2326a921ea4993ff07c11cbbcf1323c7c8d94
tposhashank <30650425+tposhashank@users.noreply.github.com>
2017-08-24 17:59:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,37 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+// Database Connection started
+
+var pool = require('pg').Pool;
+
+var config = {
+    user: '	tposhashank',
+    database: '	tposhashank',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+//Create the pool somewhere globally so it is lifetime and last for as long as app is running
+var pool = new Pool(config);
+
+app.get('/test-db', function(req,res) {
+    
+    //make a select request
+    //return the response with the results
+    pool.query('SELECT * FROM test', function (err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+});
+
+// Database Connection Ended
+
 var app = express();
 app.use(morgan('combined'));
 
